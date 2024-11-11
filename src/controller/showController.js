62779001b66cc32b169c585f90930040adb314df
luchang <chang.lu@aalto.fi>
2024-11-11 23:25:44 +0200

    get recent shows function

---
+++ src/controller/showController.js
@@ -0,0 +1,60 @@
+const e = require('express')
+const prisma = require('../service/prismaClient')
+
+async function getRecentShows(req, res) {
+	try {
+		const page = req.query.page || 1
+		const pageSize = 10
+		const time = new Date()
+		time.setHours(0, 0, 0, 0)
+		time.setDate(time.getDate() + 1)
+
+		const shows = await prisma.show.findMany({
+			take: pageSize,
+			skip: (page - 1) * pageSize,
+			orderBy: { start_time: 'desc' },
+			select: {
+				id: true,
+				name: true,
+				theater_id: true,
+				theater: {
+					select: {
+						name: true,
+					},
+				},
+				image: {
+					where: {
+						object_type: 'show',
+					},
+					select: {
+						path: true,
+					},
+				},
+			},
+			where: {
+				start_time: {
+					lt: time,
+				},
+			},
+		})
+
+		const resData = shows.map((show) => {
+			return {
+				id: show.id,
+				name: show.name,
+				theater: show.theater.name,
+				theaterId: show.theater_id,
+				imgUrl: show.image.length > 0 ? show.image[0].path : '',
+			}
+		})
+
+		res.json(resData)
+	} catch (error) {
+		console.log(error)
+		res.json('error fetching recent shows')
+	}
+}
+
+module.exports = {
+	getRecentShows,
+}
--- src/route/showRoutes.js
+++ src/route/showRoutes.js
@@ -1,8 +1,7 @@
+const showController = require('../controller/showController')
+
 const router = require('express').Router()
 
-router.get('/getRecommendedShows', (req, res) => {
-	console.log('get recommended shows route called')
-	res.send('get recommended shows route called')
-})
+router.get('/recent', showController.getRecentShows)
 
 module.exports = router
